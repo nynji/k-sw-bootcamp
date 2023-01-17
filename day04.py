0b518fb50539cf7ade1fac0e7437c934c89dc215
HJ <12210544a@inha.edu>
2023-01-17 14:12:05 +0900

    7장 연습문제

--- day04.py
+++ day04.py
@@ -1,42 +1,14 @@
-# #list
+#연습문제 7.4
+things = ["mozzarella", "cinderella", "salmonela"]
+things[-2]=things[-2].title()
+print(things)
+things[0]=things[0].upper()
+print(things)
+#things.remove("salmonela")
+#pop은 리턴값이 있어서 먼저 할 수 없다. <<-졸아서 설명 못들은듯 다시 공부하기
+print(f'{things.pop()}를 삭제 ')
+print(things)
 
-import copy
-a=[1,2,[5,-9]]
-b=copy.deepcopy(a)
-a[2][1]=7 #mutable, deepcopy(별도의 메모리 공간 할당, 각방. 명확히 복제본을 분리. 단, 메모리 많이 든다)
-print(a,b)
+#for thing in things:
+ #   print(thing[1].title())
 
-
-
-
-# a=[1,2,[5,-9]]  #mutable, b/c/d affects
-# b=a.copy()
-# c=list(a)
-# d=a[:]
-# a[2][1]=7
-# print(a,b,c,d)  ##이떈 list라 불변 아님, 따라서 a만 바꿔도 b,c,d에서도 바뀜 -> deepcopy라고 하기 어려움
-
-
-# a=[1,2,3]
-# b=a.copy()
-# c=list(a)
-# d=a[:]
-# a[2]='sw'   #immutable
-# print(a,b,c,d)  #얜 안바뀜
-
-
-
-# mixed = [6, 4, 5, 'A', 7, '트와이스', 'B', 'b','마마무']
-# mixed.sort()
-# print(mixed)
-# mixed.sort(reverse==Tu=rue)
-# lfj gu[primes;p01;1
-#
-
-
-# primes = [2, 19, 3.0, 5, 7, 11]
-# primes_sorted = sorted(primes)
-# print(primes)
-# print(primes_sorted)
-# primes.sort()
-# print(primes)
