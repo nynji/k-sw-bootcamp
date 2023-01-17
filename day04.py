a33d96c50d4d0e9ecaa448acfa9aa21b50845568
HJ <12210544a@inha.edu>
2023-01-17 15:11:25 +0900

    day04 dictionary 설명, 안주추천

--- day04.py
+++ day04.py
@@ -1,14 +1,50 @@
-#연습문제 7.4
-things = ["mozzarella", "cinderella", "salmonela"]
-things[-2]=things[-2].title()
-print(things)
-things[0]=things[0].upper()
-print(things)
-#things.remove("salmonela")
-#pop은 리턴값이 있어서 먼저 할 수 없다. <<-졸아서 설명 못들은듯 다시 공부하기
-print(f'{things.pop()}를 삭제 ')
-print(things)
+# #dictionary
+#
+# students = {'name': 'kiminha', 'age':23, 'eyes':[0.9, 1.1] }
+# #for k in students.keys():
+# #for k in students.values():
+# for k, v in students.items():
+#     #print(k)
+#     print(f'{k} : {v}')
+#
+# print(f'이름은 {students["name"]}, 나이는 {students["age"]}', end=', ')
+# print(f'시력은 좌: {students["eyes"][0]} 우: {students["eyes"][1]}')
+# import random
+#
+# #안주 추천 (dictionary)
+import random
+alcohol_foods = {
+    '맥주': ['카스', '테라'],
+    '소주': '골뱅이소면',
+    '와인': '치즈',
+    '고량주': '짬뽕'
+}
+alcohol_list = list(alcohol_foods) #키 값만 추출 extract keys
+food_list = [food for food in alcohol_foods.values()]
+print(alcohol_list, food_list)
+while True:
+    alcohol = input(f'술을 선택하세요 1) {alcohol_list[0]} 2){alcohol_list[1]} 3){alcohol_list[2]} 4){alcohol_list[3]} 5) 계산 :  6) 아무거나 ')
+    if alcohol == '5':
+        print('끝')
+        break
+    elif alcohol == '1':
+        print(f'안주 {alcohol_foods[alcohol_list[0]][random.randint(0,1)]}')
+    elif alcohol == '2':
+        print(f'안주 {alcohol_foods[alcohol_list[1]]}')
+    elif alcohol == '3':
+        print(f'안주 {alcohol_foods[alcohol_list[2]]}')
+    elif alcohol == '4':
+        print(f'안주 {alcohol_foods[alcohol_list[3]]}')
+    elif alcohol == '6':
+        #print(f'술 {alcohol_list[random.randint(0,3)]} 안주 {alcohol_foods[alcohol_list[random.randint(0,3)]]}')
+        print(f'술 {random.choice(alcohol_list)}, 안주 {random.choice(food_list)}')
+    else:
+        print('1-5 선택')
+        pass
 
-#for thing in things:
- #   print(thing[1].title())
+print('\n\n\n')
+import random
 
+number = [random.randint(0,200) for i in range(10)]
+
+print(number)
