13d05e02e89777e72a802e8d8721c67dd53cb4d0
HJ <12210544a@inha.edu>
2023-01-18 17:07:51 +0900

    연습문제 9.1 (제너레이터 이전까지)

--- day05_mission.py
+++ day05_mission.py
@@ -1,17 +1,6 @@
-#연습문제 8.11
-odd = {i for i in range(10) if i%2==1}
-print(odd)
+#연습문제 9.1
 
-#8.12 제너레이터 안배움
+def good():
+    return ['Harry', 'Ron', 'Hermione']
 
-#8.13
-key = ('optimist', 'pessimist', 'troll')
-value = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
-dic = dict(zip(key, value))
-print(dic)
-
-#8.14
-titles = ['Creature of Habit', 'Crewel Fate']
-plots = ['A nun turns into a monster', 'A haunted yarn shop']
-movies = dict(zip(titles, plots))
-print(movies)
+good()
