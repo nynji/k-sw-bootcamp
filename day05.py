622902fa420a4cd2b57be5b58afd512414ec7524
HJ <12210544a@inha.edu>
2023-01-18 15:07:02 +0900

    day05 function 03 (고차함수 관련)

--- day05.py
+++ day05.py
@@ -1,67 +1,36 @@
-# function - *
-#놀이공원 ver.1
-# import random
-# def calculate_fee(args)->list:  #*args일 땐 튜플형식이라 리스트로 입력 받고 정수와 비교할 수 없다.
-#     ## ->list : 오류 안남, return 형식 보여주는 것
-#     """
-#     놀이공원 요금 계산 프로그램
-#     :param args:ages in list
-#     :return:[전체 인원 수, 어른 수, 아이 수, 총 입장료]
-#     """
-#     total=0
-#     adults=0
-#     kids=0
-#     for age in args:
-#         if 19<=age: #adult
-#             total=total+10000
-#             adults+=1
-#         else:
-#             total=total+3000
-#             kids+=1
-#
-#     return [len(args), adults, kids, total]
-#
-# # print(calculate_fee(20,20,25))
-# # print(calculate_fee(45,43,10,7))
-#
-# no_of_visitor = int(input(('몇 분이세요? ')))
-# ages = [random.randint(1, 60) for age in range(no_of_visitor)]
-# print(ages)
-# print(calculate_fee(ages))
-# #
-# # 5q분 방문하셨고 어른 2명 아이 3명 총 요금은 ~입니다.
-# result = calculate_fee(ages)
-# print(f'{result[0]}명, 어른{result[1]}명, 아이{result[2]}명, 총 요금{result[-1]}원')
-
-#ver.2 dictionary로 리턴
-import random
-def calculate_fee(args)->dict:  #*args일 땐 튜플형식이라 리스트로 입력 받고 정수와 비교할 수 없다.
-    ## ->list : 오류 안남, return 형식 보여주는 것
+#함수의 매개변수로 함수 이름 사용 가능
+def inha():
     """
-    놀이공원 요금 계산 프로그램
-    :param args:ages in list
-    :return:[전체 인원 수, 어른 수, 아이 수, 총 입장료]
+    숫자 출력 함수
+    :return:
     """
-    total=0
-    adults=0
-    kids=0
-    for age in args:
-        if 19<=age: #adult
-            total=total+10000
-            adults+=1
-        else:
-            total=total+3000
-            kids+=1
+    print(60)
+def call_function(f):
+    """
+    매개변수로 함수를 넘겨 받아 실행
+    :param f: 매개변수가 함수
+    :return:
+    """
+    f()  # 넘겨 받은 함수 실행
+call_function(inha)
 
-    return {'no_of_people': len(args), 'no_of_adult': adults, 'no_of_kid':kids, 'total_fee': total}
-# print(calculate_fee(20,20,25))
-# print(calculate_fee(45,43,10,7))
+print(type(call_function))
 
-no_of_visitor = int(input(('몇 분이세요? ')))
-ages = [random.randint(1, 60) for age in range(no_of_visitor)]
-print(ages)
-print(calculate_fee(ages))
 #
-# 5q분 방문하셨고 어른 2명 아이 3명 총 요금은 ~입니다.
-result = calculate_fee(ages)
-print(f"전체 {result['no_of_people']}명, 어른{result['no_of_adult']}명, 아이{result['no_of_kid']}명, 총 요금{result['total_fee']}원")
+
+def subtract(n1, n2):
+    print(n1 - n2)
+
+
+def run_func(f, arg1, arg2):
+    """
+    함수를 매개 변수로 받아 함수 안에서 해당 함수를 실행
+    :param f: 첫번째 인수는 함수
+    :param arg1: 정수 값
+    :param arg2: 정수 값
+    :return:
+    """
+    f(arg1, arg2)
+
+run_func(subtract, 99, 88)
+
