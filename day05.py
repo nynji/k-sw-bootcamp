cf210f66104a4cd0abd9f703d362bc5caba7fce1
HJ <12210544a@inha.edu>
2023-01-18 16:39:43 +0900

    day05 function 05 (람함수다)

--- day05.py
+++ day05.py
@@ -1,24 +1,37 @@
-#Closures
+#Lambda
 
-def calculate():
-    x=1
-    y=2
-    temp=0
-    def add_sub(n):
-        nonlocal temp  #지역변수 아니다, 외부함수에 영향 끼침 (변경 가능)
+# 그냥 하면 다음과 같다(함수를 매개변수로 사용)
+# 함수 만들어두고 여기저기 쓸 땐 이 방법이 좋지만 그렇지 않을 경우 람다가 간단
+# import random
+#
+# def process(no_lists, f):
+#     for no in no_lists:
+#         print(f(no))
+#
+# def squares(n):
+#     """
+#     제곱 함수
+#     :param n: integer
+#     :return: integer
+#     """
+#     return n * n
+#
+# numbers = [random.randint(1, 100) for i in range(5)]
+# print(numbers)
+#
+# process(numbers, squares)
 
-        #x=11  #지역변수(local variable): 외부함수 (calculate)의 x와 다름, 변경 못함,
-                #add_sub 내에서만 쓰임
-        temp=temp + x+ n -y
-        return temp
-  #  print(x)
 
-    return add_sub
+#람다함수 사용
+import random
 
-c1 = calculate()
-for i in range(5):
-    print(c1(i))  ##왜 i가 n으로 가는거임?
+def process(no_lists, f):
+    for no in no_lists:
+        print(f(no))
 
-print(type(c1))
-print(c1)
+numbers = [random.randint(1, 100) for i in range(5)]
+print(numbers)
+
+process(numbers, lambda x: x*x)
+
 
