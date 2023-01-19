82b2ca13b42ad2c7cfe810680c45aef2932fcb18
HJ <12210544a@inha.edu>
2023-01-19 14:06:01 +0900

    day 06 Exception

--- day06.py
+++ day06.py
@@ -1,33 +1,36 @@
-def factorial_iter(n):
-    """
-    반복문을 사용한 팩토리얼 함수
-    :param n: n!
-    :return: 팩토리얼 계산 결과 값
-    """
-    result = 1
-    for k in range(1, n+1):
-        result = result*k
-    return result
+#예외
 
 
-def factorial_recu(n):
-    """
-    재귀함수를 사용한 팩토리얼 계싼 함수
-    :param n: n!
-    :return: 자기 자신을 호출 또는 1
-    """
-    if n == 1:  #끝나는 조건
-        return 1
-    else:
-        return factorial_recu(n-1) * n
-
-def something(n):
-    a = 5+n
-    print(a)
-
-something(1)
+# def div_calc(a, b):
+#     """
+#     나누기 함수
+#     :param a: 분자
+#     :param b: 분모
+#     :return: 계산결과
+#     """
+#     return a / b
+#
+# print(div_calc(15, 3))
 
 
+try:
+    raise Exception("쉬는 시간")
+    expr = input('분자 분모 입력 : ').split()
+    print(int(expr[0])/int(expr[1]))
+#vALUEeRROR
+#ZeroDivisionError
+except ValueError:
+    print('숫자 입력')
+except ZeroDivisionError as err:  #as err 하면 시스템 오류 문구 출력 가능
+    print(err)
+    print('분모에 0')
+except IndexError as err:
+    print('입력 값 범위')
+except Exception as err:
+    print(err)
+    print('무언가 문제가 발생했습니다!')
 
-print(factorial_iter(30))
-print(factorial_recu(30))
+else:  #예외 발생하지 않았을 때
+    print('프로그램 정상 종료', end=' ')
+finally:  #예외 발생 여부와 상관 없이 무조건 실행
+    print('종료')
