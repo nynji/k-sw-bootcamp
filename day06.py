47db85c2b7a96a66b2d7250af064c61c33f44987
HJ <12210544a@inha.edu>
2023-01-19 11:22:00 +0900

    day 06 전역변수

--- day06.py
+++ day06.py
@@ -1,20 +1,45 @@
-#데코레이터
+# #데코레이터
+#
+#
+# #decorator
+# def document_info(func):
+#     def new_function(*args, **kwargs):
+#         print('실행 중인 함수: ', func.__name__)
+#         print('위치 기반 인수: ', args)
+#         print('키워드 기반 인수들: ', kwargs)
+#         result = func(*args, **kwargs)
+#         print('실행 결과: ', result)
+#         return result
+#     return new_function
+#
+# @document_info
+# def sub_int(x, y):
+#     return x-y
+#
+# @document_info
+# def squares(n):
+#     return n*n
+#
+#
+# print(sub_int(7,3))
+# print(squares(5))
+# # info_sub_int = document_info(sub_int)
+# # r = info_sub_int(7,3)
+# # print(r)
 
-def sub_int(x, y):
-    return x-y
+g=1  #<-전역변수, 얘 있으면 함수 밖에서 g를 알 수 있음.
+def print_global():
+    #g= 1  #지역변수
+    print(g)
 
-#decorator
-def document_info(func):
-    def new_function(*args, **kwargs):
-        print('실행 중인 함수: ', func.__name__)
-        print('위치 기반 인수: ', args)
-        print('키워드 기반 인수들: ', kwargs)
-        result = func(*args, **kwargs)
-        print('실행 결과: ', result)
-        return result
-    return new_function
+def change_print_global():
+    global g #<-전역변수 됨
+    print(g)
+    g = 2
+    print(g)
 
-print(sub_int(7,3))
-info_sub_int = document_info(sub_int)
-r = info_sub_int(7,3)
-print(r)
+change_print_global()
+print_global()
+print(g)  #여기서 g를 알 수 없음
+print(globals())
+print(__name__)
