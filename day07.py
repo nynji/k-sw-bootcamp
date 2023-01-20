10db87b14e3c7ded48e5954f6aa56c15b6a9fc42
HJ <12210544a@inha.edu>
2023-01-20 15:39:04 +0900

    day07 도형(매직메서드)

--- day07.py
+++ day07.py
@@ -28,15 +28,14 @@ class Rectangle(Shape):
     def get_area(self):
         return self.width * self.length
 
-c1= Circle(100, 100, 10.0)
-c2=Circle(50, 50, 2.0)
-r1=Rectangle(100, 50, 5, 2)
+    def __repr__(self):
+        return f'사각형의 좌표는 x: {self.x}, y: {self.y}이고 넓이는 {self.get_area()}'
 
-print(f'사각형의 좌표는 x: {r1.x}, y: {r1.y}이고 넓이는 {r1.get_area()}')
-
-print(c1.get_area())
-print(c2.get_area())
-print(r1.get_area())
+    def __add__(self, other):
+        #두 사각형 넓이의 합
+        #return (self.width*self.length) + (other.width * other.length)
+        return Rectangle(0, 0, (self.width+other.width), (self.length+other.length))
+            #각 사각형의 width의 합과 length의 합의 곱
 
 
 class Cylinder(Circle):
@@ -46,6 +45,24 @@ class Cylinder(Circle):
 
     def get_area(self):  #get_volume
         return super().get_area() * self.height
+
+
+c1= Circle(100, 100, 10.0)
+c2=Circle(50, 50, 2.0)
+r1=Rectangle(100, 50, 5, 2)
+r2=Rectangle(70, 30, 9, 7)
+
+print(r1)
+print(r2)
+print(' ')
+print(r1+r2)
+print(' ')
+print(c1.get_area())
+print(c2.get_area())
+print(r1.get_area())
+
+
+
 
 cy1 = Cylinder(20, 20, 10.0, 2)
 print(cy1.get_area())
