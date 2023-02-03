f8afcef39f7d1922fc57648826b2787baddcef4b
HJ <12210544a@inha.edu>
2023-02-03 11:30:10 +0900

    day15 linear list 03

--- day15.py
+++ day15.py
@@ -1,24 +1,39 @@
-pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
 
 
-def insert_data(idx, pokemon):
+def add_data(pokemon):
+    pokemons.append(None)
+
+    #pokemons[len(pokemons) - 1] = pokemon
+    pokemons[-1] = pokemon
+
+
+def delete_data(idx):
     if idx < 0 or idx > len(pokemons):
-        print("Out of range!")
+        print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
-    pokemons.append(None)  # 빈칸 추가
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
 
-    for i in range(len(pokemons) - 1, idx, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
+    for i in range(idx + 1, len(pokemons)):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
 
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+    del (pokemons[len(pokemons) - 1])
+
+
+
+
 
 if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
+
     print(pokemons)
-    insert_data(2, '거북왕')
+    #pokemons.insert(3, '어니부기')
+    delete_data(1)
     print(pokemons)
-    insert_data(6, '어니부기')
+    #pokemons.insert(6, '어니부기')
+    delete_data(3)
     print(pokemons)
 
 
