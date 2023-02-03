4c8c6617ec43c8046f86374603eb6609b62a037d
HJ <12210544a@inha.edu>
2023-02-03 11:16:19 +0900

    day15 linear list 02

--- day15.py
+++ day15.py
@@ -1,17 +1,42 @@
-pokemons = list()  # 빈 배열
+pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
 
 
-def add_data(pokemon):
-    pokemons.append(None)
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
 
-    #pokemons[len(pokemons) - 1] = pokemon
-    pokemons[-1] = pokemon
+    pokemons.append(None)  # 빈칸 추가
+
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+
+if __name__ == "__main__":
+    print(pokemons)
+    insert_data(2, '거북왕')
+    print(pokemons)
+    insert_data(6, '어니부기')
+    print(pokemons)
 
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
 
-print(pokemons)
+# pokemons = list()  # 빈 배열
+#
+#
+# def add_data(pokemon):
+#     pokemons.append(None)
+#
+#     #pokemons[len(pokemons) - 1] = pokemon
+#     pokemons[-1] = pokemon
+#
+#
+# add_data('피카츄')
+# add_data('라이츄')
+# add_data('파이리')
+# add_data('꼬부기')
+# add_data('이상해')
+#
+# print(pokemons)
