eaa1fff72e6aa8126bf9598892d65f94f0abd99b
HJ <12210544a@inha.edu>
2023-02-03 14:13:19 +0900

    day15 linear list 06

--- day15.py
+++ day15.py
@@ -1,54 +1,57 @@
+## 함수 선언 부분 ##
+def print_poly(px):
+    """
+    다항식을 포맷에 맞게 출력하는 함수
+    :param px: 계수를 원소로 가지고 있는 list
+    :return: 다항식 문자열
+    """
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    poly_str = "P(x) = "
+
+    for i in range(len(px)):
+        coef = px[i]  # 계수
+
+        if coef > 0:
+            if i!=0:
+                poly_str += "+"
+        if coef == 0:
+            term-=1
+            continue
+        poly_str = poly_str + f' {coef}x^{term} '
+        term = term - 1
+
+    return poly_str
 
 
-def add_data(pokemon):
-    pokemons.append(None)
+def calc_poly(x_val, px):
+    """
+    다항식의 산술연산 계산
+    :param x_val: 계수를 원소로 가지고 있는 list
+    :param px: 다항식 계산 결과값 integer
+    :return:
+    """
+    retValue = 0
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
 
-    #pokemons[len(pokemons) - 1] = pokemon
-    pokemons[-1] = pokemon
+    for i in range(len(px)):
+        coef = px[i]  # 계수
+        retValue += coef * x_val ** term
+        term -= 1
 
-
-def delete_data(idx):
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
-        return
-
-    len_pokemons = len(pokemons)
-    pokemons[idx] = None  # 데이터 삭제
-
-    for i in range(idx, len(pokemons)):
-        #del (pokemons[- 1])
-        pokemons.pop()
-
-    temp = pokemons[:idx]
+    return retValue
 
 
+## 전역 변수 선언 부분 ##
+px = [7, -4, 0, 6]  # = 7x^3 -4x^2 +0x^1 +5x^0
+
+## 메인 코드 부분 ##
 if __name__ == "__main__":
-    pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
 
-    print(pokemons)
-    #pokemons.insert(3, '어니부기')
-    delete_data(1)
-    print(pokemons)
-    #pokemons.insert(6, '어니부기')
-    # delete_data(3)
-    # print(pokemons)
+    print(print_poly(px))
+
+    x_value = int(input("X 값-->"))
+
+    px_value = calc_poly(x_value, px)
+    print(px_value)
 
 
-
-# pokemons = list()  # 빈 배열
-#
-#
-# def add_data(pokemon):
-#     pokemons.append(None)
-#
-#     #pokemons[len(pokemons) - 1] = pokemon
-#     pokemons[-1] = pokemon
-#
-#
-# add_data('피카츄')
-# add_data('라이츄')
-# add_data('파이리')
-# add_data('꼬부기')
-# add_data('이상해')
-#
-# print(pokemons)
