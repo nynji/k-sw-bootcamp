bb477f0e0b34fc7b9a981bcecbf2498021c12203
HJ <12210544a@inha.edu>
2023-02-06 15:23:18 +0900

    day16 교ìstack

--- day16.py
+++ day16.py
@@ -1,139 +1,30 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
+stack = [None for _ in range(5)]
+top = -1
 
+top = top+1
+stack[top]= "커피"
 
-def print_nodes(start):
-    current = start
-    if current == None :
-        return
-    print(current.data, end=' ')
-    while current.link != start:  #
-        current = current.link
-        print(current.data, end=' ')
-    print()
+top=top+1
+stack[top]="녹차"
 
+top=top+1
+stack[top]="꿀물"
+top=top+1
+stack[top]="오렌지주스"
+top=top+1
+stack[top]="바닐라"
 
-def insert_nodes(find_data, insert_data):
-    global head, current, pre
-    if head.data == find_data:
-        node = Node(insert_data)
-        node.link = head
-        last = head
-        while last.link != head:
-            last = last.link
-        last.link = node
-        head = node
-        return
+#stack overflow
+# top=top+1
+# stack[top]="카라멜"
 
-    current = head
-    while current.link != head:
-        pre = current
-        current = current.link
-        if current.data == find_data:
-            node = Node(insert_data)
-            node.link = current
-            pre.link = node
-            return
+for i in range(len(stack)-1, -1, -1):
+    print(stack[i])
 
-    node = Node(insert_data)
-    current.link = node
-    node.link = head
+data = stack[top]
+stack[top] = None
+top = top-1
+print(data)
 
-
-def delete_nodes(delete_data):
-    global head, current, pre
-
-    if head.data == delete_data:
-        current = head
-        head = head.link
-        last = head  #
-        while last.link != current:  #
-            last = last.link  #
-        last.link = head  #
-        del current
-        return
-
-    current = head
-    while current.link != head:
-        pre = current
-        current = current.link
-        if current.data == delete_data:
-            pre.link = current.link
-            del current
-            return
-
-
-def find_nodes(find_data):
-    global head, current, pre
-
-    current = head
-    if current.data == find_data:
-        return current
-
-    while current.link != head:  #
-        current = current.link
-        if current.data == find_data:
-            return current
-
-    return Node(None)
-
-
-def is_find(find_data):
-    """
-    연결 리스트안에서 원소 존재 여부 판정 함수
-    :param find_data: 찾고자 하는 원소. str
-    :return: 연결 리스트안에서 원소가 존재하면 True리턴 아니면 False
-    """
-
-    global head, current, pre
-
-    current = head
-    if current.data == find_data:
-        return True
-
-    while current.link != head:  #
-        current = current.link
-        if current.data == find_data:
-            return True
-
-    return False
-
-head, current, pre = None, None, None
-data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
-
-
-if __name__ == "__main__":
-    node = Node(data_array[0])
-    head = node
-    node.link = head
-
-    for data in data_array[1:]:
-        pre = node
-        node = Node(data)
-        pre.link = node
-        node.link = head
-
-
-    print(is_find('꼬부기'))
-    print(is_find('성윤모'))
-    print(find_nodes('꼬부기').data)
-    print(find_nodes('김인하').data)
-    print(find_nodes('피카츄').data)
-
-    # print_nodes(head)
-    # delete_nodes("피카츄")
-    # print_nodes(head)
-    # delete_nodes("파이리")
-    # print_nodes(head)
-    # delete_nodes("김인하")
-    # print_nodes(head)
-
-    # print_nodes(head)
-    # insert_nodes("피카츄", "잠만보")
-    # print_nodes(head)
-    # insert_nodes("파이리", "어니부기")
-    # print_nodes(head)
-    # insert_nodes("성윤모", "거북왕")
-    # print_nodes(head)
+for i in range(len(stack)-1, -1, -1):
+    print(stack[i])
