985d0e076d43a35dbc0440351547ca1e979f9d96
HJ <12210544a@inha.edu>
2023-02-06 15:40:27 +0900

    day166 stack 02

--- day16.py
+++ day16.py
@@ -1,30 +1,65 @@
-stack = [None for _ in range(5)]
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    return False
+
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == 1:
+        return True
+    return False
+
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is Full!")
+        return
+    top = top+1
+    stack[top]=data
+
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty")
+        return
+    temp = stack[top]
+    stack[top]=None
+    top=top-1
+    return temp
+
+
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty")
+        return None
+
+    return stack[top]
+
+SIZE = int(input("Stack Size : "))
+stack = [None for _ in range(SIZE)]
 top = -1
 
-top = top+1
-stack[top]= "커피"
+if __name__ == "__main__" :
+	select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
 
-top=top+1
-stack[top]="녹차"
+	while (select != 'X' and select != 'x') :
+		if select=='I' or select =='i' :
+			data = input("입력할 데이터 ==> ")
+			push(data)
+			print("스택 상태 : ", stack)
+		elif select=='E' or select =='e' :
+			data = pop()
+			print("추출된 데이터 ==> ", data)
+			print("스택 상태 : ", stack)
+		elif select=='V' or select =='v' :
+			data = peek()
+			print("확인된 데이터 ==> ", data)
+			print("스택 상태 : ", stack)
+		else :
+			print("입력이 잘못됨")
 
-top=top+1
-stack[top]="꿀물"
-top=top+1
-stack[top]="오렌지주스"
-top=top+1
-stack[top]="바닐라"
+		select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
 
-#stack overflow
-# top=top+1
-# stack[top]="카라멜"
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
-
-data = stack[top]
-stack[top] = None
-top = top-1
-print(data)
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
+	print("프로그램 종료!")
